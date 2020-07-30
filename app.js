ef350c80426e36d21821a0bb3b9b963d996dfdc3
Justin Huang <justinhuang777@gmail.com>
2020-07-31 01:57:39 +0800

    update app.js, config.json, package.json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const helpers = require('./_helpers')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
