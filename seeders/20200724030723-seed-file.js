6f61265ceee9fe686392dfc71b57f53a6add8287
Justin Huang <justinhuang777@gmail.com>
2020-07-31 20:14:13 +0800

    add fake avatar image to seed data

--- seeders/20200724030723-seed-file.js
+++ seeders/20200724030723-seed-file.js
@@ -28,6 +28,7 @@ module.exports = {
           email: `user${i}@example.com`,
           password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
           role: 'user',
+          avatar: 'https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}',
           createdAt: new Date(),
           updatedAt: new Date()
         })
