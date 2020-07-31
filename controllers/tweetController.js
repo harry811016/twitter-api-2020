2cbf57e399082e2a6fdd0f2e9fc8f8710d42d165
Harry <harry811016@gmail.com>
2020-07-31 14:02:41 +0800

    v3

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -16,11 +16,11 @@ let tweetController = {
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         // tweetCreatedAt: moment(r.dataValues.createdAt).fromNow(),
-        // userName: r.User.name,
-        // userAvatar: r.User.avatar,
-        // userAccount: r.User.account,
-        // replyConut: r.Replies.length,
-        // likeConut: r.Likes.length,
+        userName: r.User.name,
+        userAvatar: r.User.avatar,
+        userAccount: r.User.account,
+        replyConut: r.Replies.length,
+        likeConut: r.Likes.length,
         // isLiked: r.LikedUsers.map(d => d.id).includes(helpers.getUser(req).id), //測試文件中helper無給定LikedTweets而抓不到 但因需用到map函式前面不得為undefined 故先改成從推文角度出發
         // loginUserRole: helpers.getUser(req).role,  //測試文件中helper無給定role而抓不到
       }))
